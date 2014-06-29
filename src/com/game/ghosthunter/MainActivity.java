4a7470094d5af3cf6e16d724d7664818c7335e7a
Mainak Biswas <mainakbiswas@ymail.com>
2014-06-29 15:39:29 +0530

    test commit

--- src/com/game/ghosthunter/MainActivity.java
+++ src/com/game/ghosthunter/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        // test commit
         setContentView(R.layout.main);
     }
 }
